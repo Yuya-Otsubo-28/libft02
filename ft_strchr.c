6fd3686ce57c171bd57649fd0c1178bbdeda70c1
yotsubo <513047.otsubo@gmail.com>
2024-04-03 03:34:39 +0900

    add: ft_strchr

---
+++ ft_strchr.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <stdio.h>
+
+char *ft_strchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+int	main(void)
+{
+	char str[] = "12345674890";
+	char c = '4';
+
+	printf("str: %s\n  c: %c\n\n", str, c);
+	printf("ft_strchr: %s\n", ft_strchr(str, c));
+	printf("   strchr: %s\n", strchr(str, c));
+	return (0);
+}
