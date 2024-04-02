1495315ef9af66fe44c608df2160fc6c08056c06
yotsubo <513047.otsubo@gmail.com>
2024-04-02 22:06:22 +0900

    add: ft_isprint

---
+++ ft_isprint.c
@@ -0,0 +1,14 @@
+#include <ctype.h>
+
+int	ft_isprint(int c)
+{
+	return (c >= 32 && c <= 126);
+}
+
+int	main(void)
+{
+	char c = 'a';
+	printf("ft_isprint: %d\n", ft_isprint(c));
+	printf("isprint: %d\n", isprint(c));
+	return (0);
+}
