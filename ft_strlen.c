8bc00c2e6340356f6fea5df1b4104f3cbc1c3403
yotsubo <513047.otsubo@gmail.com>
2024-04-03 02:32:38 +0900

    add: ft_strlen

---
+++ ft_strlen.c
@@ -0,0 +1,19 @@
+#include <string.h>
+
+size_t	strlen(const char *s)
+{
+	size_t i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+int	main(void)
+{
+	char *s = "Hello, World!";
+	printf("ft_strlen: %zu\n", strlen(s));
+	printf("strlen: %zu\n", strlen(s));
+	return (0);
+}
