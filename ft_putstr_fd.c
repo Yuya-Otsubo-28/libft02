67f7f7513b9bcdd6fb2cb935b7c7ed328607e130
yotsubo <513047.otsubo@gmail.com>
2024-04-04 23:48:56 +0900

    add: ft_putstr_fd

---
+++ ft_putstr_fd.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	size_t	i;
+
+	if (!s)
+		return ;
+	i = 0;
+	while (s[i])
+		write(fd, &s[i++], sizeof(char));
+}
+
+int	main(void)
+{
+	char s[] = "Hello World!\n";
+	
+	ft_putstr_fd(s, 1);
+	return (0);
+}
