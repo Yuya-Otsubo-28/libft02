6def53119daf605d8e1c0c85ebfc05d9f170b1f5
yotsubo <513047.otsubo@gmail.com>
2024-04-17 11:10:35 +0900

    add: ft_striteri.c

---
+++ ft_striteri.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+// static void	test_func(unsigned int i, char *cp)
+// {
+// 	(void)i;
+// 	*cp += 1;
+// }
+
+// char	*ft_strdup(const char *str)
+// {
+// 	size_t	i;
+// 	size_t	len;
+// 	char	*res;
+
+// 	len = 0;
+// 	while (str[len])
+// 		len++;
+// 	res = (char *)malloc(sizeof(char) * (len + 1));
+// 	if (!res)
+// 		return (NULL);
+// 	i = 0;
+// 	while (str[i])
+// 	{
+// 		res[i] = str[i];
+// 		i++;
+// 	}
+// 	res[i] = '\0';
+// 	return (res);
+// }
+
+void ft_striteri(char *s, void (* f)(unsigned int, char*))
+{
+	size_t	i;
+
+	if (!s || !f)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		f((unsigned int)i, &s[i]);
+		i++;
+	}
+}
+
+// int	main(void)
+// {
+// 	char ori[] = "";
+// 	char *s = ft_strdup(ori);
+
+// 	ft_striteri(s, &test_func);
+// 	printf("ori: %s\n", ori);
+// 	printf("  s: %s\n", s);
+// 	free(s);
+// 	return (0);
+// }
